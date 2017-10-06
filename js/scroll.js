23aa9b96f5b8a76284ba47d146b797fe9d584fd2
yukaile119@gmail.com <yukaile119@gmail.com>
2017-10-06 23:14:32 +0800

    1

---
+++ js/scroll.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('.option1').click(function(){
+        $('html,body').animate({
+            scrollTop: $('#self-intro').offset().top
+        },800);
+    });
+    $('.option2').click(function(){
+        $('html,body').animate({
+            scrollTop: $('#showskill').offset().top
+        },800);
+    });
+    $('.option3').click(function(){
+        $('html,body').animate({
+            scrollTop: $('#contactMe').offset().top
+        },800);
+    });
+});
