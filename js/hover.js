0aa717eb5fbe61b02aac27d50951616160523d85
yukaile119@gmail.com <yukaile119@gmail.com>
2017-10-08 15:16:25 +0800

    2017.10.8

---
+++ js/hover.js
@@ -0,0 +1,78 @@
+function toEdgeDist(x,y,x2,y2){
+    const xDiff = x-x2;
+    const yDiff = y-y2;
+    return (xDiff**2 + yDiff**2);
+    // 算出四边形某条边到几何中点的距离
+}
+
+//计算出最近的中点
+const closestDist = function(x,y,w,h){
+    const toTop = toEdgeDist(x,y,w/2,0);
+    const toBottom = toEdgeDist(x,y,w/2,h);
+    const toLeft = toEdgeDist(x,y,0,h/2);
+    const toRight = toEdgeDist(x,y,w,h/2);
+    const minDist = Math.min(toTop,toBottom,toLeft,toRight);
+    switch(minDist){
+        case toTop:
+          return 'top';
+        case toBottom:
+          return 'bottom';
+        case toLeft:
+          return 'left';
+        case toRight:
+          return 'right';
+    }
+}
+
+$(document).ready(function(){
+    const num = $(".boxes").length;
+    for( let i = 0;i<num; i++){
+        $('.boxes').eq(i).mouseenter(function(e){
+            const x = e.pageX - this.offsetLeft;
+            const y = e.pageY - this.offsetTop;
+            const w = this.clientWidth;
+            const h = this.clientHeight;
+            const enterResult = closestDist(x, y, w, h);
+
+            switch(enterResult){
+              case 'top':
+                $('.overlay').eq(i).css({'top' : '-100%','left':'0%'});
+                $('.overlay').eq(i).animate({top:'0%'},400);
+                break;
+              case 'bottom':
+                $('.overlay').eq(i).css({'top':'100%','left':'0%'});
+                $('.overlay').eq(i).animate({top:'0%'},400);
+                break;
+              case 'left':
+                $('.overlay').eq(i).animate({left:'0%'},400);
+                break;
+              case 'right':
+                $('.overlay').eq(i).css({'left':'100%','top':'0%'});
+                $('.overlay').eq(i).animate({left:'0%'},400);
+                break;
+            }
+        })
+        $('.boxes').eq(i).mouseleave(function(e){
+            $('overlay').eq(i).css({'left':'0','top':'0'});
+            const x = e.pageX - this.offsetLeft;
+            const y = e.pageY - this.offsetTop;
+            const w = this.clientWidth;
+            const h = this.clientHeight;
+            const exitResult = closestDist(x, y, w, h);
+            switch (exitResult){
+              case 'top':
+                $('.overlay').eq(i).animate({'top':'-100%'},400);
+                break;
+              case 'bottom':
+                $('.overlay').eq(i).animate({'top':'100%'},400);
+                break;
+              case 'left':
+                $('.overlay').eq(i).animate({'left':'-100%'},400);
+                break;
+              case 'right':
+                $('.overlay').eq(i).animate({'left':'100%'},400);
+                break;
+            }
+        })
+    }
+})
--- js/scroll.js
+++ js/scroll.js
@@ -1,17 +1,17 @@
 $(document).ready(function(){
     $('.option1').click(function(){
         $('html,body').animate({
-            scrollTop: $('#self-intro').offset().top
+            scrollTop: $('.content-section-a').offset().top
         },800);
     });
     $('.option2').click(function(){
         $('html,body').animate({
-            scrollTop: $('#showskill').offset().top
+            scrollTop: $('.content-section-b').offset().top
         },800);
     });
     $('.option3').click(function(){
         $('html,body').animate({
-            scrollTop: $('#contactMe').offset().top
+            scrollTop: $('.content-section-d').offset().top
         },800);
     });
 });
